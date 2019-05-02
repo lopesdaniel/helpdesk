152c65b69db548e7663da336e508209ea475773e
Daniel Lopes <viana.daniel02@gmail.com>
2019-05-02 17:45:53 -0300

    Commit inicial

---
+++ src/main/java/com/danieldev/api/models/User.java
@@ -0,0 +1,17 @@
+package com.danieldev.api.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+}

726ff3b19530c0b208122e50de394acf3057c9cc
Daniel Lopes <viana.daniel02@gmail.com>
2019-05-03 10:07:38 -0300

    Adicionando model role e fazendo relacionamento com users

---
+++ src/main/java/com/danieldev/api/models/Role.java
@@ -0,0 +1,54 @@
+package com.danieldev.api.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	@NotEmpty(message = "O campo não pode estar vazio...")
+	private String role;
+
+	public Role() {
+	}
+
+	public Role(Long id, String role) {
+		this.id = id;
+		this.role = role;
+	}
+
+	public Role(String role) {
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+	
+}
--- src/main/java/com/danieldev/api/models/User.java
+++ src/main/java/com/danieldev/api/models/User.java
@@ -1,12 +1,19 @@
 package com.danieldev.api.models;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.ManyToAny;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -41,6 +48,11 @@ public class User {
 	@Length(min = 5, message = "A senha não pode ter menos de 5 caracteres")
 	private Boolean active;
 	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "users_roles", 
+			   joinColumns = @JoinColumn(name = "user_id"), 
+			   inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles;
 
 	public User(Long id, String email, String name, String lastName, String password, Boolean active) {
 		this.id = id;
@@ -109,6 +121,14 @@ public class User {
 
 	public void setActive(Boolean active) {
 		this.active = active;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
 	}
 	
 	
